e8280f3a723d0d22bcc31ebf4ec49d23b21542df
WHM0304 <whm0304@naver.com>
2023-11-24 11:51:55 +0900

    알고리즘

---
+++ Java_001/src/com/callor/hello/algorithm/LoopC.java
@@ -0,0 +1,17 @@
+package com.callor.hello.algorithm;
+
+public class LoopC {
+	public static void main(String[] args) {
+		
+		for(int index =1; index <= 100; index++) {
+			System.out.print("* ");
+			if(index % 10 ==0) {
+				System.out.println();
+			} 
+			
+				
+			
+		}
+	}
+
+}
