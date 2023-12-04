c3bbd0ce85167be7d7ead3fda68d31bdb6d730c2
WHM0304 <whm0304@naver.com>
2023-12-04 16:42:03 +0900

    String , data

---
+++ Java_007/src/com/callor/oop/exec/StringBA.java
@@ -0,0 +1,18 @@
+package com.callor.oop.exec;
+
+public class StringBA {
+	
+	public static void main(String[] args) {
+		String nums = "50,88,70,100,98";
+		String[] result = nums.split(",");
+		int sum = 0;
+		for(int i = 0 ; i < result.length; i ++) {
+			System.out.println(result[i]);			
+		}
+		
+		for(int i = 0; i < result.length; i++) {
+			sum += Integer.valueOf(result[i]);
+		}
+		System.out.println("합계 : " + sum);		
+	}
+}
