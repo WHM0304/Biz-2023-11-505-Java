4e0c940cc222da31dfcf312178b4abbbc589eeae
WHM0304 <whm0304@naver.com>
2023-12-07 14:49:01 +0900

    List

---
+++ Java_010/src/com/callor/score/exec/ScoreBA.java
@@ -0,0 +1,31 @@
+package com.callor.score.exec;
+
+// 도구 import Ctrl + Shift + O
+import com.callor.score.model.ScoreDto;
+import com.callor.score.service.NumberService;
+
+public class ScoreBA {
+	
+	public static void main(String[] args) {
+		NumberService numService = new NumberService();
+		ScoreDto scoreDto = new ScoreDto();
+		
+		int start = 50;
+		int end = 100;
+		
+		int scoreKor = numService.inputNumber("국어점수를", start,end);
+		int scoreEng = numService.inputNumber("영어점수를", start,end);
+		int scoreMath = numService.inputNumber("수학점수를", start,end);
+		
+		scoreDto.kor = scoreKor;
+		scoreDto.eng = scoreEng;
+		scoreDto.math = scoreMath;
+		
+		
+		System.out.println("총점 : " + scoreDto.getTotal());
+		
+		
+		
+	}
+
+}
---
+++ Java_010/src/com/callor/score/exec/ScoreC.java
@@ -0,0 +1,51 @@
+package com.callor.score.exec;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.callor.score.model.ScoreDto;
+import com.callor.score.service.NumberService;
+
+public class ScoreC {
+
+	public static void main(String[] args) {
+		NumberService numService = new NumberService();
+		ScoreDto scoreDto = new ScoreDto();
+		List<ScoreDto> scores;
+		scores = new ArrayList<>();
+		int start = 50;
+		int end = 100;
+//		List<ScoreDto> scores = new ArrayList<ScoreDto>();
+//		scoreDto.kor = numService.inputNumber("국어점수를", start, end);
+//		scoreDto.eng = numService.inputNumber("영어점수를", start, end);
+//		scoreDto.math = numService.inputNumber("수학점수를", start, end);
+//		scores.add(scoreDto);
+//		scoreDto.kor = numService.inputNumber("국어점수를", start, end);
+//		scoreDto.eng = numService.inputNumber("영어점수를", start, end);
+//		scoreDto.math = numService.inputNumber("수학점수를", start, end);
+//		scores.add(scoreDto);
+//		scoreDto.kor = numService.inputNumber("국어점수를", start, end);
+//		scoreDto.eng = numService.inputNumber("영어점수를", start, end);
+//		scoreDto.math = numService.inputNumber("수학점수를", start, end);
+//		scores.add(scoreDto);
+//		System.out.printf("%d\t%d\t%d\t%d\n", scoreDto.kor, scoreDto.eng, scoreDto.math, scoreDto.getTotal());
+		for(int i = 0; i <3; i++) {
+			scoreDto.kor = numService.inputNumber("국어점수를", start, end);
+			scoreDto.eng = numService.inputNumber("영어점수를", start, end);
+			scoreDto.math = numService.inputNumber("수학점수를", start, end);
+			ScoreDto dto = new ScoreDto();
+			scores.add(scoreDto);
+			
+		}
+		for(int i = 0; i <scores.size(); i++) {
+			ScoreDto dto = scores.get(i);			
+			System.out.printf("%d\t",dto.kor);
+			System.out.printf("%d\t",dto.eng);
+			System.out.printf("%d\t",dto.math);
+			System.out.printf("%d\n",dto.getTotal());
+		}
+		
+		
+	}
+
+}
