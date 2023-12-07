72f3ab75c114a4055440240003b51f338fb45450
WHM0304 <whm0304@naver.com>
2023-12-07 11:51:30 +0900

    객체

---
+++ Java_010/src/com/callor/score/exec/ScoreB.java
@@ -0,0 +1,23 @@
+package com.callor.score.exec;
+
+import com.callor.score.model.ScoreDto;
+import com.callor.score.service.NumberService;
+
+public class ScoreB {
+	
+	public static void main(String[] args) {
+		
+		NumberService numService = new NumberService();
+		ScoreDto scoreDto = new ScoreDto();
+		int num1 = numService.inputNumber("국어점수를",50,100);		
+		scoreDto.kor = num1; 
+		num1 = numService.inputNumber("영어점수를", 50, 100);
+		scoreDto.eng = num1;
+		num1 = numService.inputNumber("수학점수를", 50, 100);
+		scoreDto.math = num1;
+		System.out.printf("3과목의 총점 : %d \n",scoreDto.getTotal());
+		
+		
+	}
+
+}
